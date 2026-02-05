a661176248c277c152ad7e36cd2ef195b70d2813
frozenbots60-source <frozenbots60@gmail.com>
2026-02-05 14:59:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient
 
 API_ID = 29568441
 API_HASH = "b32ec0fb66d22da6f77d355fbace4f2a"
-BOT_TOKEN = "8436171863:AAEdTIZAs00VsCM1E-2AvVKX4a93kek3sFw"
+BOT_TOKEN = "8302453295:AAFLJEUx-JAa75jbtIDLw-JelzKOPWhPs-8"
 
 SUPPORT_CHAT_LINK = "https://t.me/kustbotschat"
 UPDATES_CHANNEL_LINK = "https://t.me/kustbots"
