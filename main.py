d0d5f207ccccc3295847b894e8a5415e358756da
frozenbots60-source <frozenbots60@gmail.com>
2026-03-03 21:59:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ UPDATES_CHANNEL_LINK = "https://t.me/kustbots"
 UPI_DM_LINK = "https://t.me/KustXoffical"
 
 # --- Code Claimer Assets ---
-CLAIMER_API_URL = "https://code-auth11-4cc0b14f630c.herokuapp.com"
+CLAIMER_API_URL = "https://code-auth-432b14a97f21.herokuapp.com"
 # Forwards for Claimer (Proof channels)
 CLAIMER_FORWARD_1 = ("kustvault", 5)
 CLAIMER_FORWARD_2 = ("kustvault", 6)
