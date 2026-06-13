28269b588e1ebc0d6989bfb0eefb8a57484a0a9f
kustbotsweb-collab <kustbotsweb@gmail.com>
2026-06-13 14:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ API_CLAIMER_DEPLOYERS = [
         "deploy_id": 2,
         "url": "https://api-claimer-deploy-2-c8f46133dff1.herokuapp.com",
         "token": "fuck1234",
-        "limit": 0
+        "limit": 95
     },
     {
         "deploy_id": 3,
