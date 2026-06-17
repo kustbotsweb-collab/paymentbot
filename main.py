4792349820f86b1829b7fe6d6fa712e65c059d0e
kustbotsweb-collab <kustbotsweb@gmail.com>
2026-06-17 10:20:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ UPDATES_CHANNEL_LINK = "https://t.me/Rabit0505"
 UPI_DM_LINK = "https://t.me/Rabit0505"
 
 # --- Code Claimer Assets ---
-CLAIMER_API_URL = "https://code-auth-1-c604181235ea.herokuapp.com"
+CLAIMER_API_URL = "https://code-auth-dash-643e4005edaa.herokuapp.com"
 # Forwards for Claimer (Proof channels)
 CLAIMER_FORWARD_1 = ("rebateautomations", 2)  # 1st: the claimer file
 CLAIMER_FORWARD_2 = ("rebateautomations", 3)  # 2nd: setup video
 
 # --- API Claimer Assets ---
-API_CLAIMER_AUTH_URL = "https://code-auth-1-c604181235ea.herokuapp.com"  # Same as Code Claimer auth
+API_CLAIMER_AUTH_URL = "https://code-auth-dash-643e4005edaa.herokuapp.com"  # Same as Code Claimer auth
 
 # DEPLOY URLS (Load Distribution Pool)
 # The bot will deploy a single container using the first deployer that hasn't reached its limit.
@@ -37,7 +37,7 @@ API_CLAIMER_DEPLOYERS = [
         "deploy_id": 1,
         "url": "https://api-claimer-deploy1-a1a033817b5a.herokuapp.com",
         "token": "fuck1234",
-        "limit": 80
+        "limit": 0
     },
     {
         "deploy_id": 2,
