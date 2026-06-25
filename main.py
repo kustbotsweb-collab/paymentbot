d536d7be8d4d95827f51d0a973a2caa607f04ae7
kustbotsweb-collab <kustbotsweb@gmail.com>
2026-06-25 23:45:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from pymongo import MongoClient
 
 API_ID = 29568441
 API_HASH = "b32ec0fb66d22da6f77d355fbace4f2a"
-BOT_TOKEN = "8573345038:AAEppbd1tFOP5NEq0AzJcjBM9moDkves5eQ"
+BOT_TOKEN = "8362240967:AAFeg-ZsWbSabFRn7MaRCS6bsdp2M1e4YNk"
 
 SUPPORT_CHAT_LINK = "https://t.me/Rabit0505"
 UPDATES_CHANNEL_LINK = "https://t.me/Rabit0505"
